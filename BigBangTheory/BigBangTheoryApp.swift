173207a31e4602016d755ea1cefcd4fff1bbdaaa
Luis Cano <lukaz30@gmail.com>
2025-01-02 20:48:44 +0100

    Initial Commit

---
+++ BigBangTheory/BigBangTheoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBangTheoryApp.swift
+//  BigBangTheory
+//
+//  Created by Luis Cano  on 2025-01-02.
+//
+
+import SwiftUI
+
+@main
+struct BigBangTheoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
