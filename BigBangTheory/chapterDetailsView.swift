de117900f4b875cbd7fa2688ffa8c517d7ccb8c1
Luis Cano <lukaz30@gmail.com>
2025-01-05 12:45:09 +0100

    modificaciones varias

---
+++ BigBangTheory/chapterDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  chapterDetailsView.swift
+//  BigBangTheory
+//
+//  Created by Luis Cano  on 2025-01-04.
+//
+
+import SwiftUI
+
+struct chapterDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    chapterDetailsView()
+}
---
+++ BigBangTheory/seasonsView.swift
@@ -0,0 +1,18 @@
+//
+//  seasonsView.swift
+//  BigBangTheory
+//
+//  Created by Luis Cano  on 2025-01-04.
+//
+
+import SwiftUI
+
+struct seasonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    seasonsView()
+}
